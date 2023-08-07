d0bff242eb4903deae6ae4604c694b9db23d72d7
Benjamin Reece <b@bnjmn.org>
2023-08-07 11:25:19 -0600

    lets go

--- pages/api/postToAll.ts
+++ pages/api/postToAll.ts
@@ -1,17 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import Mastodon from 'mastodon-api';
 import { createClient } from '@supabase/supabase-js';
-import got from 'got';
-import { ThreadsAPI } from 'threads-api';
-import OAuth from 'oauth-1.0a';
-import crypto from 'crypto';
-
-// Define a custom error type
-interface GotError extends Error {
-  response: {
-    body: any;
-  };
-}
+import { TwitterApi } from 'twitter-api-v2';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
@@ -55,92 +45,29 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(500).json({ error: 'Error posting to Mastodon', details: mastodonError });
     }
 
-
-/*    // Post to Threads
-const threadsUsername = process.env.THREADS_USERNAME;
-const threadsPassword = process.env.THREADS_PASSWORD;
-
-if (!threadsUsername || !threadsPassword) {
-  return res.status(500).json({ error: 'Threads environment variables are not set' });
-}
-
-const threadsAPI = new ThreadsAPI({
-  username: threadsUsername, 
-  password: threadsPassword,
-  device: {
-    manufacturer: 'Apple',
-    model: 'MacBookPro18,4',
-    os_version: 14.0,
-    os_release: 'macOS',
-  },
-});
-
-try {
-  await threadsAPI.publish({ text: postContent });
-  console.log('Successfully posted to Threads');
-} catch (threadsError) {
-  console.error('Error posting to Threads:', threadsError);
-  return res.status(500).json({ error: 'Error posting to Threads', details: threadsError });
-}
- */
-
-    // Define OAuth 1.0a object
-    const oauth = OAuth({
-      consumer: {
-        key: process.env.TWITTER_CONSUMER_KEY,
-        secret: process.env.TWITTER_CONSUMER_SECRET
-      },
-      signature_method: 'HMAC-SHA1',
-      hash_function: (base_string: string, key: string) => crypto
-        .createHmac('sha1', key)
-        .update(base_string)
-        .digest('base64')
-    });
-  
-   // Define request data
-   const requestData = {
-    url: 'https://api.twitter.com/2/tweets',
-    method: 'POST',
-    data: {
-      status: postContent,
-    },
-  };
-    // Define the OAuth access token
-    const token = {
-      key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-      secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-    };
-  
     // Post to Twitter
+    const twitterClient = new TwitterApi({
+      appKey: process.env.TTWITTER_CONSUMER_KEY,
+      appSecret: process.env.TWITTER_CONSUMER_SECRET,
+      accessToken: process.env.TWITTER_ACCESS_TOKEN,
+      accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    });
+
     try {
-      const req = await got.post(requestData.url, {
-        body: JSON.stringify(requestData.data),
-        headers: {
-          Authorization: oauth.toHeader(oauth.authorize(requestData, token))['Authorization'],
-          'user-agent': "v2CreateTweetJS",
-          'content-type': "application/json",
-          'accept': "application/json"
-        },
-      });
-  
-      if (!req.body) {
-        throw new Error('Unsuccessful request to Twitter');
-      }
-  
-      console.log('Twitter response:', req.body);
-    } catch (error) {
-      const twitterError = error as GotError;
-      console.error('Error posting to Twitter:', twitterError.response.body);
-      return res.status(500).json({ error: 'Error posting to Twitter', details: twitterError.response.body });
+      const tweetResponse = await twitterClient.v2.tweet(postContent);
+      console.log('Twitter response:', tweetResponse);
+    } catch (twitterError) {
+      console.error('Error posting to Twitter:', twitterError);
+      return res.status(500).json({ error: 'Error posting to Twitter', details: twitterError });
     }
-  
-  res.status(200).json({ message: 'Posted to Mastodon, Twitter, and stored in DB successfully.' });
-} catch (err) {
-  console.error('An error occurred:', err);
-  if (err instanceof Error) {
-    res.status(500).json({ error: `An error occurred while processing your request: ${err.message}`, stack: err.stack });
-  } else {
-    res.status(500).json({ error: `An error occurred while processing your request: ${String(err)}` });
+
+    res.status(200).json({ message: 'Posted to Mastodon, Twitter, and stored in DB successfully.' });
+  } catch (err) {
+    console.error('An error occurred:', err);
+    if (err instanceof Error) {
+      res.status(500).json({ error: `An error occurred while processing your request: ${err.message}`, stack: err.stack });
+    } else {
+      res.status(500).json({ error: `An error occurred while processing your request: ${String(err)}` });
+    }
   }
-}
 }
